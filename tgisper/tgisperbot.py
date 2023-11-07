161cd989da82fa023b7a3370b6ce1445327911a2
Julian Dreykorn <dreykorn.julian@gmail.com>
2023-11-07 12:23:42 +0100

    Improves error handling, and adds audio messages
    This code improves documentation and error handling,
    and it adds classical audio messages instead of voice messages to the handler.

--- tgisper/tgisperbot.py
+++ tgisper/tgisperbot.py
@@ -1,75 +1,85 @@
 import logging
 import os
+import sys
 from typing import BinaryIO
 
 import ffmpeg
 import numpy as np
 import telebot
 from faster_whisper import WhisperModel
+from telebot.apihelper import ApiException
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Load configuration from environment variables
 bot_id = os.getenv("BOT_ID")
+if not bot_id:
+    logger.error("The BOT_ID environment variable not set.")
+    sys.exit(1)
+
 model_name = os.getenv("ASR_MODEL", "small")
 
+# Initialize the bot and model
 bot = telebot.TeleBot(bot_id)
 model = WhisperModel(model_name, device="cpu", compute_type="float32")
 SAMPLE_RATE = 16000
 
 
+def main():
+    # Main function to start the bot polling
+    logger.info("Starting bot polling.")
+    try:
+        bot.infinity_polling(timeout=10, long_polling_timeout=5)
+    except ApiException as e:
+        logger.error(f"APIException occurred: {e}")
+    except Exception as e:
+        logger.error(f"Unhandled exception: {e}")
+
+
 @bot.message_handler(commands=["help", "start"])
 def send_welcome(message):
+    """Send a welcome message in response to start/help commands."""
     bot.reply_to(
         message,
-        "Hello! I'm a voice recognition bot 🎤 \nRecord the voice or send it to me ↪️"
+        "Hello! I'm a voice recognition bot 🎤 Send me a voice message and I'll transcribe it for you."
     )
 
 
-@bot.message_handler(
-    content_types=["voice"],
-    chat_types=["private", "group", "supergroup"],
-)
-def transcribe_voice_message(message):
-    voice_meta = bot.get_file(message.voice.file_id)
-    voice_audio = load_audio(bot.download_file(voice_meta.file_path))
-    segments, _ = model.transcribe(
-        audio=voice_audio,
-        vad_filter=True,
-        beam_size=1,
-    )
-    text = "".join([segment.text for segment in segments])
-    bot.reply_to(message, text)
+@bot.message_handler(content_types=["voice","audio"], chat_types=["private", "group", "supergroup"])
+def handle_audio_message(message):
+    """Handle voice messages by transcribing them."""
+    try:
+        voice_meta = bot.get_file(message.voice.file_id)
+        voice_audio = load_audio(bot.download_file(voice_meta.file_path))
+        segments, _ = model.transcribe(audio=voice_audio, vad_filter=True, beam_size=1)
+        text = "".join([segment.text for segment in segments])
+        split_length = 4096
+        for start in range(0, len(text), split_length):
+            bot.send_message(message.chat.id, text[start:start + split_length])
+    except Exception as e:
+        logger.error(f"Failed to process voice message: {e}")
+        bot.reply_to(message, "Sorry, an error occurred while processing your voice message.")
 
 
-def load_audio(binary_file: BinaryIO, sr: int = SAMPLE_RATE):
-    """Read an audio file object as mono waveform, resampling as necessary.
+def load_audio(binary_file: BinaryIO, sr: int = SAMPLE_RATE) -> np.ndarray:
+    """
+    Read an audio file object as mono waveform, resampling as necessary.
 
-    Modified from https://github.com/openai/whisper/blob/main/whisper/audio.py
-    to accept a binary object.
-
-    Args:
-        binary_file (BinaryIO): The audio file like object
-        sr (int): The sample rate to resample the audio if necessary.
-
-    Raises:
-        RuntimeError: Failed to load audio
-
-    Returns:
-        np.float32: A NumPy array containing the audio waveform
+    :param binary_file: The audio file like object.
+    :param sr: The sample rate to resample the audio if necessary.
+    :return: A NumPy array containing the audio waveform.
     """
     try:
-        # This launches a subprocess to decode audio while down-mixing and
-        # resampling as necessary.
-        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
-        out, _ = (
-            ffmpeg.input("pipe:", threads=0)
-            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
-            .run(cmd="ffmpeg", capture_stdout=True, capture_stderr=True, input=binary_file)
-        )
+        out, _ = (ffmpeg
+                  .input("pipe:0", format="s16le")
+                  .output("pipe:1", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+                  .run(cmd="ffmpeg", capture_stdout=True, capture_stderr=True, input=binary_file))
     except ffmpeg.Error as e:
-        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+        logger.error(f"Failed to load audio: {e.stderr.decode().strip()}")
+        raise RuntimeError("Failed to load audio") from e
+    return np.frombuffer(out, np.int16).astype(np.float32) / 32768.0
 
-    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
-
-
-def main():
-    logging.warning("Start polling")
-    bot.infinity_polling(timeout=10, long_polling_timeout=5)
+if __name__ == "__main__":
+    main()
